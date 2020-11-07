1a1319d153a36f377f45679d30e512c8669da076
Mrat-fr <71274927+Mrat-fr@users.noreply.github.com>
2020-11-07 02:28:04 +0000

    Create Libraries - Circle.py

---
+++ Libraries - Circle.py
@@ -0,0 +1,7 @@
+import math 
+print ("Area of a circle calculate")
+print ("----------------------------")
+Q = int(input("Please Enter circle Radius: "))
+
+A = Q + math.pi
+print ("Area of a circle:",A)

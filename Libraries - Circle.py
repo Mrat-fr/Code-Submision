70719bb953ae71eeb15532af6098e013e86ed449
Mrat-fr <71274927+Mrat-fr@users.noreply.github.com>
2020-11-16 15:34:15 +0000

    Update Libraries - Circle.py

--- Libraries - Circle.py
+++ Libraries - Circle.py
@@ -1,7 +1,8 @@
+#the import command will import the math libary into the program
 import math 
 print ("Area of a circle calculate")
 print ("----------------------------")
 Q = int(input("Please Enter circle Radius: "))
-
+#This uses the pi funtion from the math libary to make it easy for this to use pi
 A = Q + math.pi
 print ("Area of a circle:",A)

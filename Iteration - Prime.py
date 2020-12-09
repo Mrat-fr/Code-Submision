bd351380373bdb52ffd4769835b532f6225cb290
Mrat-fr <71274927+Mrat-fr@users.noreply.github.com>
2020-12-09 10:09:34 +0000

    Update Iteration - Prime.py

--- Iteration - Prime.py
+++ Iteration - Prime.py
@@ -1,6 +1,7 @@
-Prime = ("2", "3", "5", "7", "11", "13", "17", "19", "23", "29")
-myit = iter(Prime)
-    
+Prime = ("2", "3", "5", "7", "11", "13", "17", "19", "23", "29")#this is a list split by commas
+myit = iter(Prime)#this splits the numbers by the commas
+
+#this will print he number after the comma
 print(next(myit))
 print(next(myit))
 print(next(myit))

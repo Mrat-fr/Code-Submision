6fb8ff36a953a5ed4ecf3abb58adafa60c79b1d2
Mrat-fr <71274927+Mrat-fr@users.noreply.github.com>
2020-12-09 10:07:24 +0000

    Update Local and Global variables - use.py

--- Local and Global variables - use.py
+++ Local and Global variables - use.py
@@ -1,10 +1,10 @@
-V = "this is a Global variable"
-X = "Global Can be used anywhere"
+V = "this is a Global variable"# this can be used anywhere
+X = "Global Can be used anywhere"#this also can
 
 def Local():
     V = "This is a Local variable"
-    print (V)
-    print (X)
+    print (V)#this can be used within this def to print the v above out
+    print (X)#this shows that it can be used anwhere
     
 print (V)
 Local()

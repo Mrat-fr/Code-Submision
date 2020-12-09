910c194b19c0882a873152798db15a254c93cfe6
Mrat-fr <71274927+Mrat-fr@users.noreply.github.com>
2020-12-09 10:00:03 +0000

    Update Selection - calculator.py

--- Selection - calculator.py
+++ Selection - calculator.py
@@ -1,18 +1,18 @@
-N1 = int(input("Please Enter First  Number: "))
+N1 = int(input("Please Enter First  Number: "))# lets you pick the numbers you want to calculate
 N2 = int(input("Please Enter Second Number: "))
-QQ = input("Please Enter The  Equation:")
+QQ = input("Please Enter The  Equation:")#pick out of + - * /
 
-if QQ == "*":
+if QQ == "*":#this is what they get if they want to times the  numbers
   AA = N1*N2
   print("Answer:",AA)
-elif QQ == "-":
+elif QQ == "-":#this is what they get if they want to minus the  numbers
   AA = N1-N2
   print("Answer:",AA)
-elif QQ == "/":
+elif QQ == "/":#this is what they get if they want to divide the  numbers
   AA = N1/N2
   print("Answer:",AA)
-elif QQ == "+":
+elif QQ == "+":#this is what they get if they want to add the  numbers
   AA = N1+N2
   print("Answer:",AA)
 else:
-  print("Input error")
+  print("Input error")#this is what they get if you did a wrong or invalid input

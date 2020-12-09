c702a8ec5e4be0990d74ae9e68aa00ebc0db117d
Mrat-fr <71274927+Mrat-fr@users.noreply.github.com>
2020-12-09 10:06:17 +0000

    Update Parameter passing - username.py

--- Parameter passing - username.py
+++ Parameter passing - username.py
@@ -1,16 +1,16 @@
-def In():
+def In():#this takes in the information to creat a username
     name = input("what is you first name?")
     sname = input("what is your second name?")
     age = input("what is your age?")
     return name, sname, age
     
-def W():
+def W():#this takes what they picked and combined then for the username
     name, sname, age=In()
     lsname = (sname[0:3])
     lname = (name[0])
     print ("your username would be:", lsname+lname + age)
     
-#---------------------------
+#---------------------------beginning
 print("--------------------")
 print ("user name generator")
 print("--------------------")

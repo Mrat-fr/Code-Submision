932b1bdbb15eb9477530133f6ac71b54810defe7
Mrat-fr <71274927+Mrat-fr@users.noreply.github.com>
2020-12-09 09:57:28 +0000

    Update Sequence - conversion.py

--- Sequence - conversion.py
+++ Sequence - conversion.py
@@ -1,5 +1,5 @@
-A = int(input("Please enter temperature in celsius: "))
-C1 = A / 5
+A = int(input("Please enter temperature in celsius: "))# lets the user input what they want converting
+C1 = A / 5 #this does the calculation
 C2 = C1 * 9
 C3 = C2 + 32
-print("This is", C3 ,"in fahrenheit")
+print("This is", C3 ,"in fahrenheit")#prints the results
